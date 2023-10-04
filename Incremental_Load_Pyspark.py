4a9045816b7a0aa3434251e93c93272b266532bf
Consultant <thompsondemetricjr@gmail.com>
2023-10-04 11:10:36 -0400

    made some changes

--- Incremental_Load_Pyspark.py
+++ Incremental_Load_Pyspark.py
@@ -22,7 +22,7 @@ database_name_hive = "fraud_project"
 table_name_hive = "fraud_full_load_external"
 
 
-table_name_postgres = "frauddetection_fullsample"
+table_name_postgres = "frauddetection_sample_milli"
 
 
 hive_query = "select max(row_id) from fraud_project.fraud_full_load_external"
@@ -30,8 +30,10 @@ hive_query = "select max(row_id) from fraud_project.fraud_full_load_external"
 max = spark.sql(hive_query)
 
 max = max.first()['max(row_id)']
+max += 1
+end_row = max + 10000
 
-postgres_query = "(select * from " + table_name_postgres +" where row_id >"+str(max)+ ") as max_table"
+postgres_query = "(select * from " + table_name_postgres +" where row_id between"+str(max)+" and " + str(end_row) + ") as max_table"
 
 df = spark.read.format("jdbc").option("url",dburl) \
     .option("driver", "org.postgresql.Driver").option("dbtable", postgres_query) \
