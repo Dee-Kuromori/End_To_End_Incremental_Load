2b5116201770f3f9c5ee444a28dcaccd18fa99a2
Consultant <thompsondemetricjr@gmail.com>
2023-10-04 11:22:22 -0400

    made some changes

--- Incremental_Load_Pyspark.py
+++ Incremental_Load_Pyspark.py
@@ -22,7 +22,7 @@ database_name_hive = "fraud_project"
 table_name_hive = "fraud_full_load_external"
 
 
-table_name_postgres = "frauddetection_sample_milli"
+table_name_postgres = "frauddetection"
 
 
 hive_query = "select max(row_id) from fraud_project.fraud_full_load_external"
